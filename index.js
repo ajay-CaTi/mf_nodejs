be47503bc73842db8ae3312e0a32692617f65d6e
ajaykumarsingh110@gmail.com <ajaykumarsingh110@gmail.com>
2023-09-16 14:42:30 +0530

    middleware and  before third party middleware

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const fs = require("fs");
 // const index = fs.readFileSync("./index.html", "utf-8");
-const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
+// const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
 // const products = data.products;
-const path = require("path");
+// const path = require("path");
 const PORT = 4000;
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  //   console.log(path.resolve(__dirname, "index.html"));
-  res.status(200).sendFile(path.resolve(__dirname, "index.html")); // can send  a file over a perticulat api
+app.use(express.json()); // builtin middleware
+app.use(express.static("public")); // builtin middleware
+// app.use(express.urlencoded()); // builtin middleware
+
+const auth = (req, res, next) => {
+  //   if (req.query.password === "123") {
+  if (req.body.password === "123") {
+    // console.log("authorize");
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+app.get("/", auth, (req, res) => {
+  res.status(200).send({ type: req.method });
+});
+
+app.post("/", (req, res) => {
+  res.status(200).send({ type: req.method });
+});
+
+app.put("/", (req, res) => {
+  res.status(200).send({ type: req.method });
+});
+
+app.delete("/", (req, res) => {
+  res.status(200).send({ type: req.method });
+});
+
+app.patch("/", (req, res) => {
+  res.status(200).send({ type: req.method });
 });
 
 app.listen(PORT, () => {
